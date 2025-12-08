74e83afd66d547162078af1c5752b543a1acdb70
stalinchidhamparam <stalinchidhamparam@gmail.com>
2025-12-08 15:15:36 +0530

    Add web support and update Firebase service exports
    Added 'web' to supported platforms in app.json and included react-dom and react-native-web dependencies for web compatibility. Updated tsconfig.json for React Native JSX and path mapping. Refactored firebase.ts to reorder and clarify exports for auth, db, and storage.

--- src/Service/firebase.ts
+++ src/Service/firebase.ts
@@ -1,12 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 import {
   initializeAuth,
   getReactNativePersistence,
 } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC_NhCA6f6FD5o7TpOXfKkdDo2a157LKQA",
@@ -15,15 +14,18 @@ const firebaseConfig = {
   storageBucket: "kindergarten-d5db8.firebasestorage.app",
   messagingSenderId: "540183968829",
   appId: "1:540183968829:web:527b4c8f93d94095a47e54",
-  measurementId: "G-V9MH5Y4V64"
+  measurementId: "G-V9MH5Y4V64",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// React Native Auth
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
+
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, storage, auth };
+export { auth, db, storage };
