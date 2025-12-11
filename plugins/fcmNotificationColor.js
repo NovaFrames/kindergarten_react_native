505d64195423384d74798278bce2b0b160c3d5a0
stalinchidhamparam <stalinchidhamparam@gmail.com>
2025-12-11 12:05:24 +0530

    Add custom FCM notification color plugin for Android
    Introduced a custom Expo config plugin (fcmNotificationColor.js) to set the default notification color for Firebase Cloud Messaging on Android. Updated app.json to use this plugin and adjusted plugin configuration for better control over notification appearance.

---
+++ plugins/fcmNotificationColor.js
@@ -0,0 +1,28 @@
+module.exports = function withFcmNotificationColor(config) {
+  return require("expo/config-plugins").withAndroidManifest(config, (config) => {
+    const androidManifest = config.modResults.manifest;
+    const app = androidManifest.application?.[0];
+
+    if (!app) return config;
+
+    if (!app["meta-data"]) {
+      app["meta-data"] = [];
+    }
+
+    app["meta-data"] = app["meta-data"].filter(
+      (item) =>
+        item.$["android:name"] !==
+        "com.google.firebase.messaging.default_notification_color"
+    );
+
+    app["meta-data"].push({
+      $: {
+        "android:name": "com.google.firebase.messaging.default_notification_color",
+        "android:resource": "@color/notification_icon_color",
+        "tools:replace": "android:resource",
+      },
+    });
+
+    return config;
+  });
+};
